abcde7fc5f6d6033857cce25d567baa50b28d0c2
Lee Solomon <leesolomon@Lees-MacBook-Pro.local>
2016-09-24 11:29:11 -0400

    template

---
+++ README.md
@@ -0,0 +1,2 @@
+Template for a simple heroku web app built with Flask, Jinja, and using bootstrap for aesthetics.
+
---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+  return render_template('home.html')
+
+@app.route('/about')
+def res():
+  return render_template('about.html')
+
+@app.route('/data')
+def com():
+  return render_template('data.html')
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return 'Sorry, page not found', 404
+
+
+
+
+if __name__ == '__main__':
+  app.run(port=33507)
